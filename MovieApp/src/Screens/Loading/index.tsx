580e64ab36c405f50986fe1d99031f5b79cd4b81
JoieHuiju <joie.huiju@gmail.com>
2020-12-18 23:01:50 +0900

    [Update] Movie app
    - 문법 오류 수정

--- MovieApp/src/Screens/Loading/index.tsx
+++ MovieApp/src/Screens/Loading/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import {ActivityIndicator} from 'react-native'; // 리액트 네이티브가 제공하는 ActivityIndicator
+import {ActivityIndicator} from 'react-native';
 import Styled from 'styled-components/native';
 
 const Container = Styled.View`
-    flex: 1;
-    background-color: #141414;
-    align-items: center;
-    justify-content: center;
+  flex: 1;
+  background-color: #141414;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Loading = () => {
-    return (
-        <Container>
-            <ActivityIndicator color="#E70915" size="large" />
-        </Container>
-    );
+  return (
+    <Container>
+      <ActivityIndicator color="#E70915" size="large" />
+    </Container>
+  );
 };
 
 export default Loading;
